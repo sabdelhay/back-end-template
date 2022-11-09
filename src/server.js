c8634aadd8240e55de2d141fdfe4602020138bbd
Sherif Abdelhay <s_abdelhay@hotmail.com>
2022-11-09 18:30:17 +0100

    Complete back-end-template

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+routes.forEach(route =>{
+  app[route.method](route.path, route.handler);
+});
+
+app.listen(8080, ()=>{
+  console.log('Server is listening on port 8080');
+});
